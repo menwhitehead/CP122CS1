ebf272d5d32373bb9840f1810ce24ffd207da14e
Matthew <matthew.whitehead@coloradocollege.edu>
2017-10-26 12:20:15 -0600

    Update formatting

--- Day4/PowerfulSpam.java
+++ Day4/PowerfulSpam.java
@@ -1,15 +1,15 @@
 public class PowerfulSpam {
 
-	public static final int SPAM_AMOUNT = 100;
+    public static final int SPAM_AMOUNT = 100;
 
     public static void main(String[] args) {
         int i = 0;
-			
+
         for (i = 0; i < SPAM_AMOUNT; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
-				
+
     }
 }
